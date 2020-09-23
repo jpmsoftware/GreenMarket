d3ec9d50e2137e51aabc0ae4528dfc50b5653813
Juan <juanpmiranda92@gmail.com>
2020-09-23 20:49:48 -0300

    corrected port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // HEROKU
 const db = mysql.createConnection({
@@ -31,8 +32,8 @@ app.use(express.static(path.join(__dirname, '/dist')));
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
